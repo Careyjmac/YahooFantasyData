20fb29f8e5bff00faaba80582775140f8554d880
Carey Halton <careyjmac@hotmail.com>
2025-10-07 12:31:15 -0400

    Update week 6 script to better account for tiebreakers, add week 5 script (to be reused for future weeks)

--- process_qb_plus_best_rb_not_flex.py
+++ process_qb_plus_best_rb_not_flex.py
@@ -22,14 +22,16 @@ winner = ""
 winner_total = 0
 winner_overall = 0
 for team in teams:
-    team_stats = query.get_team_roster_player_stats_by_week(team.team_id, week)
-    team_qb_points, team_rb_points = get_points_for_team(team_stats)
+    team_player_stats = query.get_team_roster_player_stats_by_week(team.team_id, week)
+    team_stats = query.get_team_stats_by_week(team.team_id, week)
+    team_qb_points, team_rb_points = get_points_for_team(team_player_stats)
     total = team_qb_points + team_rb_points
-    if total > winner_total or (total == winner_total and team.team_points > winner_overall):
+    if total > winner_total or (total == winner_total and team_stats["team_points"].total > winner_overall):
         winner = team.name
         winner_total = total
-        winner_overall = team.team_points
-    output.append([team.name.decode(), team_qb_points, team_rb_points, total])
+        winner_overall = team_stats["team_points"].total
+    output.append([team.name.decode(), team_qb_points, team_rb_points, total, team_stats["team_points"].total])
 
-print(tabulate(output, headers=['Team Name','QB Points','Best RB Points','Total'], tablefmt='orgtbl'))
+print(f"\nCalculating winner for week {week}, QB MP + Single RB MP (not including flex)...\n")
+print(tabulate(output, headers=['Team Name','QB Points','Best RB Points','Total','Total Overall (Tiebreaker)'], tablefmt='orgtbl'))
 print(f"\nWinner!: {winner.decode()}")
---
+++ process_random.py
@@ -0,0 +1,30 @@
+import setup
+from tabulate import tabulate
+
+query = setup.get_ff_query()
+
+week = 5
+teams = query.get_league_teams()
+
+def get_qb_points_for_team(team_player_stats):
+    for player in team_player_stats:
+        if player.selected_position_value == "QB":
+            return player.player_points_value
+
+output = list()
+winner = ""
+winner_total = 0
+winner_overall = 0
+for team in teams:
+    team_player_stats = query.get_team_roster_player_stats_by_week(team.team_id, week)
+    team_stats = query.get_team_stats_by_week(team.team_id, week)
+    team_qb_points = get_qb_points_for_team(team_player_stats)
+    if team_qb_points > winner_total or (team_qb_points == winner_total and team_stats["team_points"].total > winner_overall):
+        winner = team.name
+        winner_total = team_qb_points
+        winner_overall = team_stats["team_points"].total
+    output.append([team.name.decode(), team_qb_points, team_stats["team_points"].total])
+
+print(f"\nCalculating winner for week {week}, QB MP...\n")
+print(tabulate(output, headers=['Team Name','QB Points', 'Total(Tiebreaker)'], tablefmt='orgtbl'))
+print(f"\nWinner!: {winner.decode()}")
